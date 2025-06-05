417301bbfd11d3104b1670288110e3138e047c80
Carlos Bermúdez <cbermudezg7@gmail.com>
2025-06-04 22:44:17 -0500

    cors credentials

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(helmet({
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true,
+  // credentials: true,
 }));
 
 // Middleware para procesar JSON
